d2bea015d4efa4929db41ae72e205d29efeaa2ec
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2026-05-08 19:51:35 -0300

    feat: adição de um "banco de dados" provisorio para usuarios da plataforma

---
+++ src/data/usuarios.js
@@ -0,0 +1,46 @@
+//Arquivo provisório substitutivo de um banco de dados para usuários
+
+export const usuarios =[
+    {
+        id: 1,
+        nome: 'Beatriz Samaha',
+        email: 'beatriz.samaha@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'admin',
+    },
+    {
+        id: 2,
+        nome: 'Júlia Leal',
+        email: 'julia.leal@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'cliente',
+    },
+    {
+        id: 3,
+        nome: 'Bruna Cruz',
+        email: 'bruna.cruz@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'admin',
+    },
+    {
+        id: 4,
+        nome: 'Pedro Paulucci',
+        email: 'pedro.paulucci@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'cliente',
+    },
+    {
+        id: 5,
+        nome: 'Tiago Heitzmann',
+        email: 'tiago.heitzmann@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'admin',
+    },
+    {
+        id: 6,
+        nome: 'Tiago Lage Bacha',
+        email: 'tiago.lage@bulbeshop.com.br',
+        senha: 'senha123',
+        papel: 'cliente',
+    },
+]
