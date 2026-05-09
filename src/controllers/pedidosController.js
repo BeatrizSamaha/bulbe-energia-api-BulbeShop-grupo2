7a07457f556cbcf2f80f9ae549d6a4e4c6c10791
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2026-05-09 15:42:48 -0300

    feat: codigo de controller buscarPedidoPorId

---
+++ src/controllers/pedidosController.js
@@ -0,0 +1,22 @@
+import { pedidos } from "../data/pedidos";
+
+export const buscarPedidoPorId = (req, res) =>{
+    const { id } = req.params;
+
+    const usuarioLogadoId = req.usuario.id;
+
+    const pedido = pedidos.find(p => p.id === Number(id));
+
+    //Validação: EXISTE
+    if(!pedido){
+        return res.status(404).json({mensagem: "Pedido não encontrado"});
+    }
+
+    //Validação: Pedido é do Usuário
+    if(pedido.usuarioId !== Number(usuarioLogadoId)){
+        return res.status(403).json({mensagem: "Acesso negado: este pedido não pertence a você"})
+    }
+
+    //Sucesso
+    return res.status(200).json(pedido);
+}
