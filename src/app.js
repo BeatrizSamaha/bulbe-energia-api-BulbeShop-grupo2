3c2d676ce2d6896fa017a3c16d9cf59350666b71
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2026-05-09 15:55:19 -0300

    feat: Inclusão da rota no app

--- src/app.js
+++ src/app.js
@@ -1,44 +1,45 @@
-import express from 'express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import express from "express";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
-import cuponsRouter from './routes/cupons.js';
+// Import das ROTAS
+import cuponsRouter from "./routes/cupons.js";
+import lojasRouter from "./routes/lojas.js";
+import authRouter from "./routes/auth.js";
+import pedidosRouter from "./routes/pedidos.js";
 
 const app = express();
 app.use(express.json());
 
 const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Bulbe Energia API',
-      version: '1.0.0',
-      description: 'API do Projeto Bulbe Energia',
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Bulbe Energia API",
+            version: "1.0.0",
+            description: "API do Projeto Bulbe Energia",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
     },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
-  },
-  apis: ['./src/routes/*.js'],
+    apis: ["./src/routes/*.js"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+app.use("/api/v1/cupons", cuponsRouter);
 
-app.use('/api/v1/cupons', cuponsRouter);
+app.use("/api/v1/lojas", lojasRouter);
 
-import lojasRouter from './routes/lojas.js';
+app.use("/api/v1/auth", authRouter);
 
-app.use('/api/v1/lojas', lojasRouter);
-
-import authRouter from './routes/auth.js';
-
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/pedidos", pedidosRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
