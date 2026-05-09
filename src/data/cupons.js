ffacb1eddf0d3c18f467aa644e4b5d99bcb8a12d
Tiago Heitzmann <tiagoheitzmann@gmail.com>
2026-05-09 09:41:36 -0300

    refactor: separa mock de dados em arquivos individuais por entidade

---
+++ src/data/cupons.js
@@ -0,0 +1,5 @@
+export const cupons = [
+    { id: 1, codigo: 'BEMVINDO10', valor: 10.00, ativo: true },
+    { id: 2, codigo: 'SOLAR20', valor: 20.00, ativo: true },
+    { id: 3, codigo: 'EXPIRADO5', valor: 5.00, ativo: false }
+];
