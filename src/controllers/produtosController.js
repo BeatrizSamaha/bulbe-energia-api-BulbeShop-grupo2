431429617113f581824d47029733bf6d6ef01878
Júlia Leal <julia.borges201@gmail.com>
2026-05-09 16:10:03 -0300

    fix: correção dos campos para padrão da US-01 [IMP-01]

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -2,7 +2,18 @@ import { produtos } from '../data/produtos.js';
 
 export const listarProdutos = (req, res) => {
   try {
-    res.status(200).json(produtos);
+    const { busca } = req.query;
+
+    let resultado = produtos;
+
+    if (busca) {
+        const termo = busca.toLowerCase();
+        resultado = resultado.filter((produto) =>
+            produto.title.toLowerCase().includes(termo)
+        );
+    }
+
+    res.status(200).json(resultado);
   } catch (error) {
     res.status(500).json({ erro: 'Erro interno ao buscar produtos' });
   }
